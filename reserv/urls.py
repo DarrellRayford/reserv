6553fde5bc8cfc9535e0bf41db9e3090c4bf9d8b
darrellrayford <darrellrayford@gmail.com>
2022-09-30 12:34:49 -0500

    mysql config

--- reserv/urls.py
+++ reserv/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reserv import views as reservViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', reservViews.home),
+
 ]
